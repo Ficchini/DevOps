0154967ff33b7b72e9181ae4640cd1c9e01e66c6
JFicchini <jficchini.dev@gmail.com>
2026-03-17 08:57:02 -0300

    Atualização da página Sobre

--- src/app.py
+++ src/app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"mensagem": "API funcionando corretamente"}
+    return {"mensagem": "Sistema de Gerenciamento de Biblioteca"}
 
 @app.route("/status")
 def status():
-    return {"status": "API online"}
+    return {"status": "Sistema desenvolvido em Flask para estudo de CI/CD"}
 
 if __name__ == "__main__":
     app.run(debug=True)
