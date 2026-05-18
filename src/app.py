913cc6f12728a392052b48b57b5d68e0ce4bc075
JFicchini <jficchini.dev@gmail.com>
2026-05-18 10:35:47 -0300

    chore: prepara aplicação para o deploy

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -28,3 +29,5 @@ def cadastro_livro():
 
 if __name__ == "__main__":
     app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
