a3dc91a999a748c05b7efbc72bd8e4e386b3100a
JFicchini <jficchini.dev@gmail.com>
2026-05-18 10:12:16 -0300

    fix: corrige regra

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,7 @@
 from app import app
 
 def test_home():
-    client = app.est_client()
+    client = app.test_client()
+    assert client is not None
     response = client.get("/")
     assert response.status_code == 200
