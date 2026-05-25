c29cf32881f3c2018c5a9aeddf207e0051153d21
JFicchini <jficchini.dev@gmail.com>
2026-05-25 09:39:56 -0300

    update: criação das funções de teste no test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from app import app
 
 def test_home():
@@ -5,3 +9,35 @@ def test_home():
     assert client is not None
     response = client.get("/")
     assert response.status_code == 200
+    
+def test_status():
+    client = app.test_client()
+    assert client is not None
+    response = client.get("/status")
+    assert response.status_code == 200
+    assert response.json == {"Sistema desenvolvido em Flask para estudo de CI/CD"}
+    
+def test_livros():
+    client = app.test_client()
+    assert client is not None
+    response = client.get("/livros")
+    assert response.status_code == 200
+    
+def test_autores():
+    client = app.test_client()
+    assert client is not None
+    response = client.get("/autores")
+    assert response.status_code == 200
+    
+def test_contato():
+    client = app.test_client()
+    assert client is not None
+    response = client.get("/contato")
+    assert response.status_code == 200
+    
+def test_cadastro_livro():
+    client = app.test_client()
+    assert client is not None
+    response = client.get("/cadastro-livro")
+    assert response.status_code == 200
+    assert response.json == {"Formulário de cadastro de livros"}
